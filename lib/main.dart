9ec8ec9fb9caf4b66a159c3fa11a2e69b90b555a
Tiago Almeida <thyagoeap@gmail.com>
2019-09-22 11:34:38 -0300

    add local storage function

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'models/item.dart';
 
@@ -9,7 +12,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Todo",
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false, //hide banne debug on upper right side of screen
       theme: ThemeData(
         primarySwatch: Colors.blue
       ),
@@ -23,9 +26,9 @@ class HomePage extends StatefulWidget {
 
   HomePage(){
     items = [];
-    items.add(Item(title: "Todo 1", done: false));
-    items.add(Item(title: "Todo 2", done: true));
-    items.add(Item(title: "Todo 3", done: false));
+    // items.add(Item(title: "Todo 1", done: false));
+    // items.add(Item(title: "Todo 2", done: true));
+    // items.add(Item(title: "Todo 3", done: false));
   }
 
   @override
@@ -34,6 +37,52 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   var newTodoCtrl = TextEditingController();
+
+  _HomePageState () {
+    load();
+  }
+
+  void save() async {
+    var prefs = await SharedPreferences.getInstance();
+    await prefs.setString('data', jsonEncode(widget.items));
+  }
+
+  void add() {
+    //valid if input text is empty
+    if(newTodoCtrl.text.trim().isEmpty) return;
+
+    setState(() {
+      widget.items.add(
+        Item(
+          title: newTodoCtrl.text,
+          done: false
+        )
+      );
+    });
+    newTodoCtrl.clear();
+    save();
+  }
+
+  void remove(int index) {
+    setState(() {
+      widget.items.removeAt(index);
+    });
+    save();
+  }
+
+  Future load () async {
+    var prefs = await SharedPreferences.getInstance();
+    var data = prefs.getString('data');
+
+    if(data != null) {
+      Iterable decoded = jsonDecode(data);
+      List<Item> result = decoded.map((x) => Item.fromJson(x)).toList();
+
+      setState(() {
+        widget.items = result;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -59,14 +108,25 @@ class _HomePageState extends State<HomePage> {
         itemCount: widget.items.length,
         itemBuilder: (BuildContext context, int index) {
           final item = widget.items[index];
-          return CheckboxListTile(
+          return Dismissible(
+            direction: DismissDirection.startToEnd,
             key: Key(item.title),
-            title: Text(item.title),
-            value: item.done,
-            onChanged: (value) {
-              setState(() {
-                item.done = value;
-              });
+            child: CheckboxListTile(
+              title: Text(item.title),
+              value: item.done,
+              onChanged: (value) {
+                setState(() {
+                  item.done = value;
+                });
+                save();
+              },
+            ),
+            background: Container(
+              color: Colors.redAccent,
+              child: Icon(Icons.restore_from_trash),
+            ),
+            onDismissed: (direction) {
+              remove(index);
             },
           );
         }
@@ -77,20 +137,5 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.pinkAccent,
       ),
     );
-  }
-
-  void add() {
-    //valid if input text is empty
-    if(newTodoCtrl.text.trim().isEmpty) return;
-
-    setState(() {
-      widget.items.add(
-        Item(
-          title: newTodoCtrl.text,
-          done: false
-        )
-      );
-    });
-    newTodoCtrl.clear();
   }
 }
